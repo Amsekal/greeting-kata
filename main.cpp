07e734c18bed5fc7f7d39261a6451785156b2172
Semendi Adam <adam.semendi@gmail.com>
2018-02-28 11:27:56 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
-
+#include <string.h>
 using namespace std;
 void csillag()
 {
     for(int i=0;i<80;i++)
         cout<<"*";
 }
+void kozepre(int a)
+{
+   int y=0;
+   y=18+a;
+   for(int i=0;i<(80-y)/2;i++)
+        cout<<" ";
+}
+
 int main()
 {
-    int vannev;
+    int vannev,a;
     char nev[50];
     cin>>vannev;
 
-    if(vannev){cin>>nev;
+    if(vannev){cin>>nev;a=strlen(nev);
     csillag();
-    cout << "*Good morning " <<nev<<"!*"<< endl;
+    kozepre(a);
+   // cout << "*Good morning " <<nev<<"!*"<< endl;
 
     cout << "*Happy Eastern, " <<nev<<"!*"<< endl;
     csillag();}
     else    {
         csillag();
-    cout << "*Good morning my friend " <<"!*"<< endl;
+        kozepre(10);
+    //cout << "*Good morning my friend " <<"!*"<< endl;
 
     cout << "*Happy Eastern, my friend " <<"!*"<< endl;
     csillag();}

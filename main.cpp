f53f21269ac4f695ef5f784359e75cb858560331
Semendi Adam <adam.semendi@gmail.com>
2018-02-28 11:06:37 +0200

    greeting step 1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 
 using namespace std;
-
+void csillag()
+{
+    for(int i=0;i<80;i++)
+        cout<<"*";
+}
 int main()
 {
     char nev[50];
     cin>>nev;
+    csillag();
     cout << "Good morning " <<nev<<"!"<< endl;
 
     cout << "Happy Eastern, " <<nev<<"!"<< endl;
+    csillag();
     return 0;
 }
